56c0c979de597bcbbec7b584c4e8a17ab841004a
HasnaFitriyaniK <hasna.fitriyani.23@gmail.com>
2025-02-20 00:49:25 +0700

    security keyloak unfinished

---
+++ api-gateway/src/main/java/com/programming/techie/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.programming.techie.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    // private final String[] freeResourceUrls = {"/swagger-ui.html",
+    // "/swagger-ui/**", "/v3/api-docs/**",
+    // "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("https://your-auth-server.com/.well-known/jwks.json").build();
+    }
+}
